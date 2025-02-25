56f04f339ea2cba456cc9c0d3429c259c1790f04
doniyor209 <doniyortoshev123@gmail.com>
2025-02-25 16:58:01 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,15 @@
+let ism1=prompt("String typedagi malumot kiriting")
+
+console.log("Ism:" +ism1);
+
+let a=+prompt("Number typedagi malumot kiriting ")
+
+console.log(a+ " Yosh muborak");
+
+let as=prompt("Bolen typedagi malumot kiriting")
+
+console.log("Ism:" +as);
+
+confirm("hamma malumotlarni to'g'ri kiritganizga ishonchingiz komilmi?")
+
+alert(`Siz kiritgan ${ ism1} so'zi ${typeof ism1} typega tegishli \n Siz kiritgan ${ a} so'zi ${typeof a} typega tegishli \n Siz kiritgan ${ as} so'zi ${typeof as} typega tegishli \n`)
